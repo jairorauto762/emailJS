bbad20aeac27b7c0760d799c66eb988525ae9bf7
jairorauto762 <jairorauto.202300762@gmail.com>
2024-10-03 10:01:40 +0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+ // Initialize EmailJS with your user ID
+ (function(){
+    emailjs.init("VubtxBpYF_5R2I-Ci");
+})();
+
+
+// Function to send email using EmailJS
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Send the email using the form data
+    console.log(this)
+    emailjs.sendForm('service_6nmjtmn', 'template_bz4h6g7', this)
+        .then(function() {
+            alert('Message Sent Successfully!');
+        }, function(error) {
+            alert('Failed to send the message: ' + JSON.stringify(error));
+        });
+});
